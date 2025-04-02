41056b0f5eef6e4bebd4ff3d68e7b0a629da79db
Niall.Russell <niall.russell@ul.ie>
2025-04-02 12:51:51 +0100

    edit assignment 3

--- assignments/assignment03-cso.py
+++ assignments/assignment03-cso.py
@@ -1,7 +1,12 @@
 import requests 
+import json 
+
+filename = "cso.json"
 
 url = "https://ws.cso.ie/public/api.restful/PxStat.Data.Cube_API.ReadDataset/FIQ02/JSON-stat/2.0/en"
 response = requests.get(url)
 
-with open ("cso.json", "w") as f:
-    f.write(response.text)
+csoJSON = response.json()
+
+with open (filename, "w") as fp:
+    json.dump(csoJSON, fp, indent = 4)
